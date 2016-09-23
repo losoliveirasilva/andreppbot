47df989466cff377466455a44a4d557a54c5dbfc
Lucas Bordignon <lucaspbordignon99@gmail.com>
2016-09-22 22:10:28 -0300

    Bot send random messages after 8 hours.

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import logging
-import re
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime
+from telegram.ext import Updater, MessageHandler, Filters
 from random import choice
 
 words = (
@@ -24,18 +24,27 @@ APPNAME = os.environ.get('APPNAME')
 PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(TOKEN)
 updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-updater.bot.setWebhook("https://"+APPNAME+".herokuapp.com/"+TOKEN)
+updater.bot.setWebhook("https://" + APPNAME + ".herokuapp.com/" + TOKEN)
+
+# Loads external messages, from bot_phrases.txt, to bot.
+with open('bot_phrases.txt', 'r') as file:
+    automatic_phrases = file.read().strip().split('\n')
 
 
 def echo(bot, update):
+    last_message_hour = 0
     if update.message.from_user.username == "andremesquita96":
+        last_message_hour = datetime.now().hour
         user_message = update.message.text
-        if (re.search(r'[?]', user_message).group() == '?'):
+        if '?' in user_message:
             bot.sendMessage(update.message.chat_id, text='{}, {}'.format(
                             'Responde', choice(words)))
         else:
             bot.sendMessage(update.message.chat_id, text='{}, {}'.format(
                             user_message, choice(words)))
+    elif (last_message_hour + 8) % 24 == datetime.now().hour:
+        last_message_hour = datetime.now().hour
+        bot.sendMessage(update.message.chat_id, text=choice(automatic_phrases))
 
 updater.dispatcher.add_handler(MessageHandler([Filters.text], echo))
 updater.idle()
