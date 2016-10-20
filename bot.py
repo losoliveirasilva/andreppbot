25d7d739e051d15422f76d2898d10d7f24e4fed1
caiopo <caiopoliveira@gmail.com>
2016-10-20 19:43:07 -0200

    General improvements and bug fixes :pizza:

--- bot.py
+++ bot.py
@@ -9,21 +9,23 @@ TOKEN = os.environ.get('TOKEN')
 APPNAME = os.environ.get('APPNAME')
 PORT = int(os.environ.get('PORT', '5000'))
 updater = Updater(TOKEN)
-jobQueue = updater.job_queue
+job_queue = updater.job_queue
 updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 updater.bot.setWebhook("https://" + APPNAME + ".herokuapp.com/" + TOKEN)
 
 # Loads external messages, from bot_phrases.txt, to bot.
-with open('bot_phrases.txt', 'r') as file:
-    automatic_phrases = file.read().strip().split('\n')
+with open('bot_phrases.txt', 'r') as phrases_file:
+    automatic_phrases = phrases_file.read().strip().split('\n')
 
 # Loads external messages, from bot_swearwords.txt, to bot.
-with open('bot_swearwords.txt', 'r') as file:
-    automatic_swearwords = file.read().strip().split('\n')
+with open('bot_swearwords.txt', 'r') as swearwords_file:
+    automatic_swearwords = swearwords_file.read().strip().split('\n')
 
+max_ = 3
 
 def echo(bot, update):
-    max_ = 3
+    global max_
+
     if update.message.from_user.username == "andremesquita96":
         user_message = update.message.text
 
@@ -36,7 +38,7 @@ def echo(bot, update):
                                 user_message, choice(automatic_swearwords)))
 
         if max_ < 6:
-            max_ = max_ + 1
+            max_ += 1
         else:
             max_ = 3
 
@@ -44,7 +46,7 @@ def echo(bot, update):
 def automatic_message(bot, update, delay):
     delayed_message = Job(bot.sendMessage(update.message.chat_id, text=choice(automatic_phrases)),
                           delay, repeat=False)
-    jobQueue.put(delayed_message, next_t=delay)
+    job_queue.put(delayed_message, next_t=delay)
 
 updater.dispatcher.add_handler(MessageHandler([Filters.text], echo))
 updater.idle()
